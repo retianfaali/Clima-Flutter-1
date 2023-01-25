a6bb907ac93c4f2bdeeae52b157aab9e57ba1840
Retian Faali <muhammadretianfaali@gmail.com>
2023-01-25 12:56:03 +0700

    first commit

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,30 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
+const apiKey = '660cd7b9a53dda8f38d72ee25c35a9f0';
+const openWeatherMapURL = 'http://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+  Future<dynamic> getCityWeather(String city) async {
+    var url = '$openWeatherMapURL?q=$city&appid=$apiKey&units=metric';
+    NetworkHelper networkHelper = NetworkHelper(url);
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
@@ -20,11 +46,11 @@ class WeatherModel {
   }
 
   String getMessage(int temp) {
-    if (temp > 25) {
+    if (temp > 25.0) {
       return 'It\'s 🍦 time';
-    } else if (temp > 20) {
+    } else if (temp > 20.0) {
       return 'Time for shorts and 👕';
-    } else if (temp < 10) {
+    } else if (temp < 10.0) {
       return 'You\'ll need 🧣 and 🧤';
     } else {
       return 'Bring a 🧥 just in case';
